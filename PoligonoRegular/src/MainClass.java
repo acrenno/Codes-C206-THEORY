8bbdcdca7441508bb60b15bc9cba2b107a27715c
Anna Rennó <110424006+acrenno@users.noreply.github.com>
2022-09-01 12:48:35 -0300

    Add files via upload

---
+++ PoligonoRegular/src/MainClass.java
@@ -0,0 +1,10 @@
+public class MainClass {
+    public static void main(String[] args) {
+        //Criando um objeto
+        PoligonoRegular pr = new PoligonoRegular();
+        pr.calculaArea(54,7);
+        System.out.println(pr.getAreaPoligono());
+    }
+
+
+}
