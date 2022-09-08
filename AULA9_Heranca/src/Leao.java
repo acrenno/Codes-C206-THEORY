e27796815a988d4dcca4339a13ac9e777a802e12
Anna Rennó <110424006+acrenno@users.noreply.github.com>
2022-09-08 14:36:15 -0300

    Add files via upload

---
+++ AULA9_Heranca/src/Leao.java
@@ -0,0 +1,17 @@
+public class Leao extends Animal {
+
+    private float tamanhoJuba;
+
+    @Override
+    public void fazBarulho() {
+        System.out.println("Roar!");
+    }
+
+    public float getTamanhoJuba() {
+        return tamanhoJuba;
+    }
+
+    public void setTamanhoJuba(float tamanhoJuba) {
+        this.tamanhoJuba = tamanhoJuba;
+    }
+}
